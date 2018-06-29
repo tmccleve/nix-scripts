4bab2a0890e010f5a74af4c7a3a438ff875fa2c2
TJ McCleve <tyler@mccleve.me>
2018-06-29 09:16:09 -0600

    iptables replacement for firewalld

---
+++ cent7iptables.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo yum -y install iptables-services && sudo systemctl stop firewalld && sudo systemctl start iptables && sudo systemctl disable firewalld && sudo systemctl mask firewalld && sudo systemctl enable iptables
